d2a79924f0782364ee720db3def8a4b64c2d9202
cfdebug <rfadventure22@gmail.com>
2022-10-02 20:36:36 -0500

    Added Delete Route

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express')
 
+// DEPENDENCIES
+const methodOverride = require('method-override')
+
 // CONFIGURATION
 require('dotenv').config()
 const PORT = process.env.PORT
@@ -11,6 +14,7 @@ app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
 
 // ROUTES
 app.get('/', (req,res) => {
