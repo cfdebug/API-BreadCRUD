5d5a0fac109d4e75a41adffa46adc1f2f8abf8a3
cfdebug <rfadventure22@gmail.com>
2022-10-06 19:03:51 -0500

    Created Mongoose Connection and Schema

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require('express')
 
 // DEPENDENCIES
 const methodOverride = require('method-override')
+const mongoose = require('mongoose')
 
 // CONFIGURATION
 require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true},() => {console.log('Connected To Mongo: ',process.env.MONGO_URI)})
+
 
 // MIDDLEWARE
 app.set('views', __dirname + '/views')
