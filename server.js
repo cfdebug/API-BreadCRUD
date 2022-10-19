77cc59ddeff2d8fcea795aff472bc67b7717fd60
cfdebug <rfadventure22@gmail.com>
2022-10-18 20:30:29 -0500

    Modified HomePage Redirect

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(methodOverride('_method'))
 // ROUTES
 app.get('/', (req,res) => {
     // res.send('Welcome to an Awesome App about Breads!')
-    res.redirect('/breads')
+    res.redirect('/breads/')
 })
 
 // Breads
